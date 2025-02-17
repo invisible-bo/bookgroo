396c5d574df24f397dddb95c2cbb4814fde88d55
DESKTOP-LR64C9E\USER <sararhdns7517@naver.com>
2025-02-18 01:56:17 +0900

    feature:이메일 인증 및 model 수정

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -6,7 +6,11 @@ from django.contrib.auth.hashers import make_password
 class UserSerializers(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = "__all__"
+        fields = ["username", "password", "email","nickname"] 
+
+        extra_kwargs = {
+            "password": {"write_only": True} 
+        }
 
     def create(self, validated_data):
         validated_data["password"] = make_password(validated_data["password"])
