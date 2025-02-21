d6d4d3a8933705b7bd3590a33500a2b1bd1bebe5
invisible-bo <qhrms111@gmail.com>
2025-02-21 14:47:43 +0900

    ⚡️ Add profile page js

--- frontend/scripts/include.js
+++ frontend/scripts/include.js
@@ -14,13 +14,16 @@ document.addEventListener("DOMContentLoaded", function () {
         });    
 
         
-    function setupHeader() {
+    function setupHeader() {   //헤더 기능 설정
         const logoutBtn = document.getElementById("logout-btn");  // 로그아웃 버튼 가져오기
+        const profileNavItem = document.querySelector("nav ul li a[href='profile.html']");  // 프로필 링크
 
-        if (localStorage.getItem("access_token")) {       //local storage에 토큰 존재 여부 체크
+        if (localStorage.getItem("access_token")) {   //local storage에 토큰 확인, 로그인 상태 확인
             logoutBtn.style.display = "block";  //로그인 상태면 로그아웃 버튼 표시
+            profileNavItem.style.display = "block";  // 프로필 링크 표시
         } else {
             logoutBtn.style.display = "none";  //비로그인 상태면 숨김
+            profileNavItem.style.display = "none";  // 프로필 링크 숨김
         }
 
         // 로그아웃 버튼 클릭 이벤트
