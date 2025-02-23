af81837e25f7a35db391a0d7ff61f0bbc158d29d
invisible-bo <qhrms111@gmail.com>
2025-02-23 22:39:20 +0900

    📝 Chatbot.html basic frames

--- frontend/scripts/togglebtn.js
+++ frontend/scripts/togglebtn.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
+
     const toggleBtn = document.querySelector(".toggle-btn");
     const chatHistory = document.querySelector(".chat-history");
 
+    // 사이드바 열기/닫기
     toggleBtn.addEventListener("click", function () {
-        chatHistory.classList.toggle("open"); // open 클래스 추가/제거
+        chatHistory.classList.toggle("open");
     });
-});
+});    
