30bfae211770ffe30770fad53fa30d344330b91d
DESKTOP-LR64C9E\USER <sararhdns7517@naver.com>
2025-02-24 17:07:04 +0900

    수정

--- bookgroo/settings.py
+++ bookgroo/settings.py
@@ -111,7 +111,7 @@ REST_FRAMEWORK = {
 
 # JWT 설정 (유효기간 및 Refresh Rotation)
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=300),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
