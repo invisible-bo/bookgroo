39ce620f2366e1d3b5eb1109a2bebc931e3705c0
JaceJung-dev <jacejung.dev@gmail.com>
2025-02-24 20:34:56 +0900

    refactoring

--- chatrooms/serializers.py
+++ chatrooms/serializers.py
@@ -1,11 +1,20 @@
 from rest_framework import serializers
 from .models import Chatroom, Message
+from django.contrib.auth import get_user_model
+
+
+# class UserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = get_user_model()
+#         fields = "__all__"
+
 
 class ChatRoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Chatroom
         fields = "__all__"
-        
+
+
 class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
