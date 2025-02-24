a953420a6e98aa525460e8d8b4bab052df0b68cd
DESKTOP-LR64C9E\USER <sararhdns7517@naver.com>
2025-02-24 17:44:28 +0900

    feat:장르 시리얼라이저 및 api 추가

--- accounts/utils.py
+++ accounts/utils.py
@@ -1,10 +1,19 @@
 import threading
+from accounts.models import User
 
 class EmailThread(threading.Thread):
-  
     def __init__(self, email):
         self.email = email
         threading.Thread.__init__(self)
 
     def run(self):
         self.email.send()
+        
+def get_user_preferred_genres(user_id):
+    
+    try:
+        user = User.objects.get(id=user_id)
+        preferred_genres = user.preferred_genres.all()
+        return [genre.name for genre in preferred_genres]
+    except User.DoesNotExist:
+        return []  #사용자가 없으면
