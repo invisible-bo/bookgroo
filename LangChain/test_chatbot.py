f28e87099b514d02e535ed6fec5d6895b20473ea
jhmang1128 <jhmang1128@gmail.com>
2025-02-25 18:13:30 +0900

    update prompt

--- LangChain/test_chatbot.py
+++ LangChain/test_chatbot.py
@@ -3,12 +3,6 @@ from langchain_chroma import Chroma
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from dotenv import load_dotenv
 
-from langchain_core.prompts import (
-    ChatPromptTemplate,
-    SystemMessagePromptTemplate,
-    HumanMessagePromptTemplate,
-)
-
 def chatbot(user_question):
     load_dotenv()
 
@@ -25,81 +19,184 @@ def chatbot(user_question):
     retriever = book_DB.as_retriever()
 
     #### LLM
-    llm = ChatOpenAI(model="gpt-4o-mini")
+    llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
     
     #### prompt (gpt-4o 23년 10월)
-    prompt_01 = ChatPromptTemplate.from_template(
-            """
-                Question: {user_question}
-                Context: {context}
-                Answer:
+    prompt_A = ChatPromptTemplate.from_template(
+        """
+            Question: {user_question}
+            Context: {context}
+            Answer:
                 
-                - 상황 A : 기본
-                    1. 당신은 책을 추천해주는 봇 입니다.
-                    2. 모든 질문에 책을 추천할 필요는 없습니다. 사용자가 책 추천과 관련된 질문에 대해서만 책 추천을 진행하세요.
-                    3. 사용자가 책 추천을 요청하지 않은 경우, 책 추천을 유도하세요.
+            - 상황 A : 기본
+                1. 당신은 책을 추천해주는 봇 입니다.
+                2. 모든 질문에 책을 추천할 필요는 없습니다. 사용자가 책 추천과 관련된 질문에 대해서만 책 추천을 진행하세요.
+                3. 사용자가 책 추천을 요청하지 않은 경우, 책 추천을 유도하세요.
+                4. 사용자의 질문은 제거 해주세요.
+            
+            
+                예시 1
+                    질문 1: 아무책이나 추천해줘
+                    답변 1: 평소에 선호하시던 장르에 한해서 추천해드릴까요?
+                    
+                    질문 2: 아니 그냥 아무거나 추천해줘
+                    답변 2: 상황 B 실행
+                    
+                예시 2
+                    질문 1: 졸리다
+                    답변 1: 졸릴 때 책을 읽으면 잠이 덜 오는 경우가 있어요. 책을 추천해드릴까요?
+                    
+                    질문 2: 아니 그냥 졸려
+                    답변 2: 저는 책을 추천해주는 챗봇입니다. 다른 컨텐츠에 대해서는 답변이 어려워요.
+                    
+                    질문 3 : 그냥 대화하는 기능은 없어?
+                    답변 3 : 네 저는 책 추천만 해드릴 수 있습니다.
+                    
+                    질문 4 : 음.. 소설 아무거나 추천해줘
+                    답변 4 : 상황 B 실행
+                    
+                예시 3
+                    질문 1: 뭐해
+                    답변 1: 저는 사용자 분을 기다립니다. 책을 추천해드릴까요?
+                    
+                    질문 2: 그럼 판타지 책 추천해줘
+                    답변 2: 상황 B 실행
+                    
+                예시 4
+                    질문 1: 로멘스 추천해줘
+                    답변 1: 상황 B 실행
+                    
+                예시 5
+                    질문 1: 문학 추천해줘
+                    답변 1: 상황 B 실행
+                    
+                예시 6
+                    질문 1: 뭐 재밌는거 없나?
+                    답변 1: 재밌는 책을 추천해드릴까요?
+                    
+                    질문 2: ㅇㅇ 추천해줘
+                    답변 1: 상황 B 실행
+                    
+                    
+            - 상황 B : 책 추천
+                1. 저자, 출판사, 출판일자 등 책에 대한 정보도 함께 제공해주세요.
+                2. 책 제목 옆에 RAG 태그를 붙여주세요.
+                3. 2권 추천해주세요.
                 
-                    예시 1
-                        질문 1: 아무책이나 추천해줘
-                        답변 1: 평소에 선호하시던 장르에 한해서 추천해드릴까요?
-                        
-                        질문 2: 아니 그냥 아무거나 추천해줘
-                        답변 2: 상황 B 실행
-                        
-                    예시 2
-                        질문 1: 졸리다
-                        답변 1: 졸릴 때 책을 읽으면 잠이 덜 오는 경우가 있어요. 책을 추천해드릴까요?
-                        
-                        질문 2: 아니 그냥 졸려
-                        답변 2: 저는 책을 추천해주는 챗봇입니다. 다른 컨텐츠에 대해서는 답변이 어려워요.
-                        
-                        질문 3 : 그냥 대화하는 기능은 없어?
-                        답변 3 : 네 저는 책 추천만 해드릴 수 있습니다.
-                        
-                        질문 4 : 음.. 소설 아무거나 추천해줘
-                        답변 4 : 상황 B 실행
-                        
-                    예시 3
-                        질문 1: 뭐해
-                        답변 1: 저는 사용자 분을 기다립니다. 책을 추천해드릴까요?
-                        
-                        질문 2: 그럼 판타지 책 추천해줘
-                        답변 2: 상황 B 실행
-                        
-                    
-                - 상황 B : 책 추천
-                    1. 저자, 출판사, 출판일자 등 책에 대한 정보도 함께 제공해주세요.
-                    2. 추천 A 타입에서 2권, 추천 B 타입에서 2권, 총 4권을 추천해주세요.
-                    
-                    
-                - 추천 A 타입 :
-                    1. context를 사용해서 책을 추천해주세요.
-                    2. 첵 제목 옆에 RAG 태그를 붙여주세요.
-                
-                - 추천 B 타입 :
-                    1. context를 사용하지 않고 책을 추천해주세요.
-                    2. 첵 제목 옆에 LLM 태그를 붙여주세요.
+        """)
 
-                
-            """
-        )
+    # 추천 A 타입에서 2권, 추천 B 타입에서 2권, 총 4권을 추천해주세요.
 
-# 3. 책 제목 옆에 context에서 검색된 책에는 RAG 태그, 사용하지 않은 책에는 LLM 태그를 붙여주세요.
+    prompt_B = ChatPromptTemplate.from_template(
+        """
+            Question: {user_question}
+            Answer:
+                
+            - 상황 A : 기본
+                1. 당신은 책을 추천해주는 봇 입니다.
+                2. 모든 질문에 책을 추천할 필요는 없습니다. 사용자가 책 추천과 관련된 질문에 대해서만 책 추천을 진행하세요.
+                3. 사용자가 책 추천을 요청하지 않은 경우, 책 추천을 유도하세요.
+                4. 사용자의 질문은 제거 해주세요.
+            
+            
+                예시 1
+                    질문 1: 아무책이나 추천해줘
+                    답변 1: 평소에 선호하시던 장르에 한해서 추천해드릴까요?
+                    
+                    질문 2: 아니 그냥 아무거나 추천해줘
+                    답변 2: 상황 B 실행
+                    
+                예시 2
+                    질문 1: 졸리다
+                    답변 1: 졸릴 때 책을 읽으면 잠이 덜 오는 경우가 있어요. 책을 추천해드릴까요?
+                    
+                    질문 2: 아니 그냥 졸려
+                    답변 2: 저는 책을 추천해주는 챗봇입니다. 다른 컨텐츠에 대해서는 답변이 어려워요.
+                    
+                    질문 3 : 그냥 대화하는 기능은 없어?
+                    답변 3 : 네 저는 책 추천만 해드릴 수 있습니다.
+                    
+                    질문 4 : 음.. 소설 아무거나 추천해줘
+                    답변 4 : 상황 B 실행
+                    
+                예시 3
+                    질문 1: 뭐해
+                    답변 1: 저는 사용자 분을 기다립니다. 책을 추천해드릴까요?
+                    
+                    질문 2: 그럼 판타지 책 추천해줘
+                    답변 2: 상황 B 실행
+                    
+                예시 4
+                    질문 1: 로멘스 추천해줘
+                    답변 1: 상황 B 실행
+                    
+                예시 5
+                    질문 1: 문학 추천해줘
+                    답변 1: 상황 B 실행
+                    
+                예시 6
+                    질문 1: 뭐 재밌는거 없나?
+                    답변 1: 재밌는 책을 추천해드릴까요?
+                    
+                    질문 2: ㅇㅇ 추천해줘
+                    답변 1: 상황 B 실행
+                    
+                    
+            - 상황 B : 책 추천
+                1. 저자, 출판사, 출판일자 등 책에 대한 정보도 함께 제공해주세요.
+                2. 책 제목 옆에 LLM 태그를 붙여주세요.
+                3. 2권 추천해주세요.
+                
+        """)
+    
+    prompt_C = ChatPromptTemplate.from_template(
+        """
+            result_RAG : {result_RAG}
+            result_LLM : {result_LLM}
+            Answer:
+                
+            1. 당신은 추천 결과들을 취합하는 텍스트 편집 봇 입니다.
+            2. 중복된 내용이 있는 경우, 중복된 내용을 제거해주세요.
+            3. 다른 부가적인 설명 없이 취합된 결과만을 제공해주세요.
+            
+            
+            - 상황 A : 일상 적인 대화
+                2개의 답변을 하나의 답변으로 만들어주세요.
+            
+            - 상황 B : 책 추천 결과 취합
+                책에 대한 추천 내용은 누락 없이 모두 취합해주세요.
+            
+        """)
+
+# 2. result_RAG 2권, result_LLM의 2권, 총 4권에 대한 정보를 취합해주세요.
+
     #### RAG
     retrieved_docs = retriever.invoke(user_question)
     context = "\n".join([doc.page_content for doc in retrieved_docs])
     
+    print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
     ### chain
-    chain = prompt_01 | llm
-    res = chain.invoke({"context": context, "user_question": user_question})
-
-    # chain = prompt_01 | llm
-    # res = chain.invoke({"user_question": user_question})
+    chain_A = prompt_A | llm
+    res_A = chain_A.invoke({"context": context, "user_question": user_question})
+    result_RAG = res_A.content
+    print(result_RAG)
     
-    return res.content
+    print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+    chain_B = prompt_B | llm
+    res_B = chain_B.invoke({"user_question": user_question})
+    result_LLM = res_B.content
+    print(result_LLM)
+    
+    print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+    chain_C = prompt_C | llm
+    res_C = chain_C.invoke({"result_RAG": result_RAG, "result_LLM": result_LLM})
+    result_03 = res_C.content
+    print(result_03)
+    
+    return result_03
 
-user_question = "으어어어 재미없다"
-# user_question = "음.. 그럼 판타지 장르의 책을 추천해줘"
+# user_question = "재미 없다"
+user_question = "로멘스 하나 추천해줘봐"
 bot_message = chatbot(user_question)
-print(bot_message)
+# print(bot_message)
 
